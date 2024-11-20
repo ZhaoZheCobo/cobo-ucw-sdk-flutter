30d8af0d8438b1c1908259b40655503f9cde8da8
ZhaoZheCobo <139831276+ZhaoZheCobo@users.noreply.github.com>
2024-11-20 14:43:51 +0800

    update swift

--- ios/Classes/UcwSdkPlugin.swift
+++ ios/Classes/UcwSdkPlugin.swift
@@ -4,16 +4,17 @@ import TSSSDK
 
 public class UcwSdkPlugin: NSObject, FlutterPlugin {
   private var connInstance: TssConnection?
+  private var logInstance: TssLogger?
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "ucw_sdk", binaryMessenger: registrar.messenger())
     let instance = UcwSdkPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
 
-    let logInstance = TssLogger()
+    instance.logInstance = TssLogger()
     let logChannel = FlutterEventChannel(name: "ucw_sdk/logs", binaryMessenger: registrar.messenger())
-    logChannel.setStreamHandler(logInstance)
-    TssSetLogger(logInstance)
+    logChannel.setStreamHandler(instance.logInstance)
+    TssSetLogger(instance.logInstance)
 
 
     instance.connInstance = TssConnection()
